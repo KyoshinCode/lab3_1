59ac33735acaa9c658668042149253afacb82fc7
Robert Pakowski <ropak@wp.pl>
2017-03-21 18:56:59 +0100

    * Partial preparation and test case for reservation repository test call

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -7,13 +7,18 @@ import org.mockito.Mock;
 import org.mockito.internal.util.reflection.Whitebox;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
 import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
 import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
 import pl.com.bottega.ecommerce.system.application.SystemContext;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by 195035 on 21.03.2017.
@@ -36,6 +41,8 @@ public class AddProductCommandHandlerTest {
 
     private AddProductCommandHandler handler;
 
+    private AddProductCommand command;
+
     @Before
     public void setUp() throws Exception {
         handler = new AddProductCommandHandler();
@@ -44,5 +51,15 @@ public class AddProductCommandHandlerTest {
         Whitebox.setInternalState(handler, "suggestionService", suggestionService);
         Whitebox.setInternalState(handler, "clientRepository", clientRepository);
         Whitebox.setInternalState(handler, "systemContext", systemContext);
+
+        command = new AddProductCommand(Id.generate(), Id.generate(), 5);
+    }
+
+    @Test
+    public void testHandle_CheckReservationRepositoryCalledCorrectly() throws Exception {
+        when(reservationRepository.load(command.getOrderId())).thenReturn(new Reservation());
+        when(productRepository.load(command.getProductId())).thenReturn(new Product());
+
+        handler.handle(command);
     }
 }
