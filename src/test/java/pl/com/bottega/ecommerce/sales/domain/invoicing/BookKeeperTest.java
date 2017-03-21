dd936f0b4f14f44b5075e8b6f08a64276d79f9bf
Aleksander Kaczmarczyk <194972@edu.p.lodz.pl>
2017-03-21 11:02:09 -0700

    Zapisanie zmianw BookKeeperTest wykonanych na zajeciach

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,14 +1,45 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class BookKeeperTest {
-
+	
 	@Test
 	public void testInvoiceRequestWithOneItemShouldReturnInvoiceWithOneLine() {
-		fail("Not yet implemented");
+		
+		InvoiceFactory factory = new InvoiceFactory();
+		BookKeeper bookKeeper = new BookKeeper(factory);
+		
+		InvoiceRequest request = mock(InvoiceRequest.class);
+		RequestItem item = mock(RequestItem.class);
+		request.add(item);
+		List<RequestItem> items = new ArrayList<RequestItem>();
+		items.add(item);
+		System.out.println(request);
+		when(request.getItems()).thenReturn(items);
+		
+		System.out.println(items);
+		
+		TaxPolicy taxPolicy = mock(TaxPolicy.class);
+		System.out.println(taxPolicy);
+		
+		//Invoice invoice = bookKeeper.issuance(request, taxPolicy);
+		Invoice invoice = mock(Invoice.class);
+		//when(invoice.getItems()).thenReturn(value)
+		Assert.assertThat(invoice.getItems().size(), is(equalTo(1)));
+		
+		
 	}
 	
 	@Test
