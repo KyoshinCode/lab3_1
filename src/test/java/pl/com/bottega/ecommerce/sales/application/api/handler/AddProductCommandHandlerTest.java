016c3143eb9c7f1045e643916f870a2ea957f48c
Robert Pakowski <ropak@wp.pl>
2017-03-21 18:40:17 +0100

    + Added AddProductCommandHandlerTest class and test fixture setup

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,48 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 195035 on 21.03.2017.
+ */
+public class AddProductCommandHandlerTest {
+
+    @Mock
+    private ReservationRepository reservationRepository;
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private SuggestionService suggestionService;
+    @Mock
+    private ClientRepository clientRepository;
+    @Mock
+    private SystemContext systemContext;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    private AddProductCommandHandler handler;
+
+    @Before
+    public void setUp() throws Exception {
+        handler = new AddProductCommandHandler();
+        Whitebox.setInternalState(handler, "reservationRepository", reservationRepository);
+        Whitebox.setInternalState(handler, "productRepository", productRepository);
+        Whitebox.setInternalState(handler, "suggestionService", suggestionService);
+        Whitebox.setInternalState(handler, "clientRepository", clientRepository);
+        Whitebox.setInternalState(handler, "systemContext", systemContext);
+    }
+}
