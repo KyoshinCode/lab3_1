ea34295c6d0b0bd347c4bacffd841e697179b599
Andrzej Borzecki <andrew.borzecki@gmail.com>
2017-03-25 00:45:20 +0100

    isolation Added test class for BookKeeper. class, added first test case(one position invoice issue)

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,65 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by grusz on 24.03.2017.
+ */
+public class BookKeeperTest {
+
+    private InvoiceRequest invoiceRequest;
+    private ArrayList<RequestItem> requestItems;
+    private RequestItem requestItem;
+    private InvoiceFactory invoiceFactory;
+
+    @Before
+    public void setUp() throws Exception {
+
+        invoiceRequest = mock(InvoiceRequest.class);
+        when(invoiceRequest.getClientData()).thenReturn(new ClientData(Id.generate(), "Andrew"));
+        requestItems = new ArrayList<>();
+        ProductData productData = mock(ProductData.class);
+        when(productData.getType()).thenReturn(ProductType.FOOD);
+        requestItem = mock(RequestItem.class);
+        when(requestItem.getTotalCost()).thenReturn(new Money(100));
+        when(requestItem.getProductData()).thenReturn(productData);
+        when(requestItem.getQuantity()).thenReturn(1);
+        when(invoiceRequest.getItems()).thenReturn(requestItems);
+        ClientData clientData = invoiceRequest.getClientData();
+        invoiceFactory = mock(InvoiceFactory.class);
+        when(invoiceFactory.create(clientData)).thenReturn(new Invoice(Id.generate(), clientData));
+    }
+
+    @Test
+    public void issueInvoice_oneRequestItem() {
+
+        requestItems.add(requestItem);
+
+        BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+        TaxPolicy taxPolicy = new TaxPolicy() {
+            @Override
+            public Tax calculateTax(ProductType productType, Money net) {
+                return new Tax(new Money(0), "no tax");
+            }
+        };
+
+        Invoice result = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        Assert.assertThat(result.getItems().size(), is(equalTo(1)));
+
+
+    }
+}
