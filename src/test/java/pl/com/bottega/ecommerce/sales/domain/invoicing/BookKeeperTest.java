f686cbe0d150120c378663d536002f90e26bc518
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-25 22:23:48 +0100

    [isolation]Dodanie before test akcji

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,5 +1,16 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +18,30 @@ import static org.junit.Assert.*;
  */
 public class BookKeeperTest {
 
+    private InvoiceFactory invoiceFactory;
+    private InvoiceRequest invoiceRequest;
+    private RequestItem requestItem;
+    private ArrayList<RequestItem> requestItemArrayList;
+
+    @Before
+    public void setUp() throws Exception{
+        invoiceRequest = Mockito.mock(InvoiceRequest.class);
+        Mockito.when(invoiceRequest.getClientData()).thenReturn(new ClientData(Id.generate(), "TEST_USER"));
+        requestItemArrayList = new ArrayList<>();
+
+        ProductData productData = Mockito.mock(ProductData.class);
+        Mockito.when(productData.getType()).thenReturn(ProductType.STANDARD);
+
+        requestItem = Mockito.mock(RequestItem.class);
+        Mockito.when(requestItem.getProductData()).thenReturn(productData);
+        Mockito.when(requestItem.getQuantity()).thenReturn(2);
+        Mockito.when(requestItem.getTotalCost()).thenReturn(new Money(200));
+
+        Mockito.when(invoiceRequest.getItems()).thenReturn(requestItemArrayList);
+
+        ClientData clientData = invoiceRequest.getClientData();
+
+        invoiceFactory = Mockito.mock(InvoiceFactory.class);
+        Mockito.when(invoiceFactory.create(clientData)).thenReturn(new Invoice(Id.generate(), clientData));
+    }
 }
