648ab41e86b89d529ee78a27079cba427f8c354b
Maciek <miedzianowski.maciek@gmail.com>
2017-03-26 14:48:47 +0200

    isolation/ Dodanie testu zachowania dla metody issuance

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -5,6 +5,7 @@ import org.mockito.Matchers;
 import org.mockito.Mockito;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
@@ -13,8 +14,7 @@ import java.util.Date;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by 195016 on 3/21/2017.
@@ -23,6 +23,7 @@ public class BookKeeperTest {
 
     private final ClientData ANY_CLIENT_DATA = new ClientData(Id.generate(), "anyName");
     private final InvoiceRequest INVOICE_REQUEST_WITH_ONE_ELEMENT = new InvoiceRequest(ANY_CLIENT_DATA);
+    private final InvoiceRequest INVOICE_REQUEST_WITH_TWO_ELEMENTS = new InvoiceRequest(ANY_CLIENT_DATA);
     private final Money ANY_MONEY = new Money(0);
     private final RequestItem ANY_REQUEST_ITEM = new RequestItem(new ProductData(Id.generate(), ANY_MONEY, "name", ProductType.FOOD, new Date()),0,ANY_MONEY);
 
@@ -48,4 +49,26 @@ public class BookKeeperTest {
 
     }
 
+    @Test
+    public void shouldCallCalculateTaxTwice() throws Exception {
+        //given
+        INVOICE_REQUEST_WITH_TWO_ELEMENTS.add(ANY_REQUEST_ITEM);
+        INVOICE_REQUEST_WITH_TWO_ELEMENTS.add(ANY_REQUEST_ITEM);
+
+        InvoiceFactory mockInvoiceFactory = mock(InvoiceFactory.class);
+        when(mockInvoiceFactory.create(ANY_CLIENT_DATA)).thenReturn(new Invoice(Id.generate(), ANY_CLIENT_DATA));
+
+        BookKeeper bookKeeper = new BookKeeper(mockInvoiceFactory);
+        TaxPolicy mockTaxPolicy = mock(TaxPolicy.class);
+        Tax tax = new Tax(ANY_MONEY, "anyDescription");
+
+        when(mockTaxPolicy.calculateTax(Mockito.any(ProductType.class), Mockito.any(Money.class))).thenReturn(tax);
+
+        //when
+        bookKeeper.issuance(INVOICE_REQUEST_WITH_TWO_ELEMENTS, mockTaxPolicy);
+
+        //then
+        verify(mockTaxPolicy, times(2)).calculateTax(Mockito.any(ProductType.class), Mockito.any(Money.class));
+
+    }
 }
