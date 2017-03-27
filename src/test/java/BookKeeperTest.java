362c6681121e502cc7851b782988ca5b5940ab46
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-27 20:10:01 +0200

    isolation Change in first BookKeeper test: stub TaxPolicy using Mockito.

--- src/test/java/BookKeeperTest.java
+++ src/test/java/BookKeeperTest.java
@@ -2,6 +2,12 @@ import org.junit.Before;
 import org.junit.Test;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.BookKeeper;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Tax;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.TaxPolicy;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Wojciech Szczepaniak on 27.03.2017.
@@ -17,6 +23,9 @@ public class BookKeeperTest {
 
     @Test
     public void requestForAnInvoiceWithOneElementReturnInvoiceContainsOneElement() {
-
+        // given
+        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class)))
+                .thenReturn(new Tax(Money.ZERO, "test"));
     }
 }
