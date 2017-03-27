7f641cc9b6ee9a4e1f9cbc329ac1ec8ec1afeb6a
mateusz.sadowski <195054>
2017-03-27 23:32:07 +0200

    Created ReservationBuilder

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/reservation/ReservationBuilder.java
@@ -0,0 +1,37 @@
+package pl.com.bottega.ecommerce.sales.domain.reservation;
+
+import java.util.Date;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+public class ReservationBuilder {
+	
+	private Reservation.ReservationStatus rStat = null;
+	private ClientData data;
+	private Id id = Id.generate();
+	
+	public ReservationBuilder withClientData(ClientData data) {
+		this.data = data;
+		return this;
+	}
+	
+	public ReservationBuilder withId(Id id) {
+		this.id = id;
+		return this;
+	}
+	
+	public ReservationBuilder close() {
+		rStat = Reservation.ReservationStatus.CLOSED;
+		return this;
+	}
+	
+	public ReservationBuilder open() {
+		rStat = Reservation.ReservationStatus.OPENED;
+		return this;
+	}
+	
+	public Reservation build() {
+		return new Reservation(id, rStat, data, new Date());
+	}
+}
