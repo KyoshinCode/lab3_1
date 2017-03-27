b28c8e8e3ef7c773d7c7664643fb51d52bd541f6
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-03-28 01:30:20 +0200

    invoiceWithOneItemTest added

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,6 +1,9 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.any;
 
 import java.sql.Date;
 
@@ -16,17 +19,29 @@ public class BookKeeperTest {
 	
 	private ClientData clientData = new ClientData(Id.generate(), "Jan");
 	private InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
-	private ProductData productData = new ProductData(Id.generate(), new Money(100), "Apple", ProductType.FOOD, Date.valueOf("2017"));
-	private RequestItem requestItem = new RequestItem(productData, 0, new Money(100));
+	private ProductData productData = new ProductData(Id.generate(), new Money(100), "Apple", ProductType.FOOD, new Date(0));
 
 	@Test
 	public void invoiceWithOneItemTest() {
+		InvoiceFactory invoiceFcatory = new InvoiceFactory();
+		BookKeeper bookKeeper = new BookKeeper(invoiceFcatory);
+		
+		TaxPolicy taxPolicyMocked = mock(TaxPolicy.class);
+		
+		when(taxPolicyMocked.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(100), "EUR"));
+		
+		RequestItem requestItem = new RequestItem(productData, 1, new Money(100));
+		invoiceRequest.add(requestItem);
+		
+		Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicyMocked);
+		assertThat(invoice.getItems().size(), is(equalTo(1)));
+		
 		
 	}
 	
-	@Test
-	public void invoiceWithTwoItemsCallCalculateTaxTwoTimesTest() {
-		
-	}
+//	@Test
+//	public void invoiceWithTwoItemsCallCalculateTaxTwoTimesTest() {
+//		
+//	}
 
 }
