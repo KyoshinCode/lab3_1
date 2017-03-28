ad398dc91707b2013549e89dea1bf9ab2fea5009
aleksander muszynski <195024@edu.p.lodz.pl>
2017-03-28 17:36:29 +0200

    created state test productAddedToClosedReservationThrowsException()

--- src/test/java/lab3_2/TAddProductCommandHandler.java
+++ src/test/java/lab3_2/TAddProductCommandHandler.java
@@ -20,6 +20,7 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
 import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
+import pl.com.bottega.ecommerce.sharedkernel.exceptions.DomainOperationException.DomainOperationException;
 
 public class TAddProductCommandHandler {
 
@@ -53,5 +54,33 @@ public class TAddProductCommandHandler {
 
 		Mockito.verify(sS, Mockito.times(0)).suggestEquivalent(p1, c);
 	}
-	
+	 
+	@Test (expected = DomainOperationException.class)
+	public void productAddedToClosedReservationThrowsException() {
+		AddProductCommandHandler handler = new AddProductCommandHandler();
+		ReservationRepository rR = Mockito.mock(ReservationRepository.class);
+		ClientRepository cR = Mockito.mock(ClientRepository.class);
+		ProductRepository pR = Mockito.mock(ProductRepository.class);
+		SuggestionService sS = Mockito.mock(SuggestionService.class);
+		AddProductCommand command;
+
+		Whitebox.setInternalState(handler, "reservationRepository", rR);
+		Whitebox.setInternalState(handler, "clientRepository", cR);
+		Whitebox.setInternalState(handler, "productRepository", pR);
+		Whitebox.setInternalState(handler, "suggestionService", sS);
+
+		Product p1 = new Product(Id.generate(), new Money(5), "test", ProductType.FOOD);
+		Product p2 = new Product(Id.generate(), new Money(8), "test4", ProductType.FOOD);
+		Reservation res = new Reservation(Id.generate(), Reservation.ReservationStatus.CLOSED,
+				new ClientData(Id.generate(), "troszke mniej zdenerwowany tester"), new Date());
+		command = new AddProductCommand(Id.generate(), Id.generate(), 123);
+		Client c = new Client();
+
+		Mockito.when(pR.load(command.getProductId())).thenReturn(p1);
+		Mockito.when(rR.load(command.getOrderId())).thenReturn(res);
+		Mockito.when(cR.load(Mockito.any(Id.class))).thenReturn(c);
+		Mockito.when(sS.suggestEquivalent(Mockito.any(Product.class), Mockito.any(Client.class))).thenReturn(p2);
+
+		handler.handle(command);
+	}
 }
