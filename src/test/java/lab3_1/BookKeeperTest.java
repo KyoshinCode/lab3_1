ff826f9cba83b7fed8c445f770c58f2cf3810818
Miłosz Szyrner <milosz.szyrner@gmail.com>
2017-03-28 08:19:16 +0200

    isolation: Zmiana modyfikatora dostepu klasy RequestItem

--- src/test/java/lab3_1/BookKeeperTest.java
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -1,14 +1,28 @@
 package lab3_1;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.BookKeeper;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceRequest;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.RequestItem;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.TaxPolicy;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeperTest {
 
 	@Test
 	public void testInvoiceRequestWithOnePositionShouldReturnInvoiceWithOnePosition() {
-		
+
 	}
 
 }
