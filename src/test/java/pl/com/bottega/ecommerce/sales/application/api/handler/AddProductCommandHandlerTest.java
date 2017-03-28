8029b9d8bd8652cddeebf5db5efaeaec8a402a1a
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-28 13:47:23 +0200

    Create AddProductCommandHandler class test and prepare configuration before tests.

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,27 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Wojciech Szczepaniak on 28.03.2017.
+ */
+public class AddProductCommandHandlerTest {
+
+    private AddProductCommandHandler addProductCommandHandler;
+
+    @Before
+    public void init() {
+        addProductCommandHandler = new AddProductCommandHandler();
+    }
+
+    @Test
+    public void test() {
+        // given
+
+        // when
+
+        // then
+
+    }
+}
