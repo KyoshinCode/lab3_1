6d87aa0f4432f06341d1947d583f3866e19a90cc
Robert Jankowski <jankowski.r@hotmail.com>
2017-03-28 16:47:23 +0200

    Add test to check if invoicing with two items call TaxPolicy twice

--- src/test/java/tests/TestInvoice.java
+++ src/test/java/tests/TestInvoice.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To change this license header, choose License Headers in Project Properties. To change this template file, choose
+ * Tools | Templates and open the template in the editor.
  */
 package tests;
 
@@ -10,11 +9,11 @@ import java.util.Date;
 import static org.hamcrest.core.Is.is;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.mockito.Matchers;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.stub;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
@@ -38,30 +37,36 @@ public class TestInvoice {
     private BookKeeper bookKeeper;
     private TaxPolicy policy;
     private Invoice inv;
-    private InvoiceFactory factory;
+    private InvoiceFactory factory = new InvoiceFactory();
     private final InvoiceRequest request = new InvoiceRequest(new ClientData(Id.generate(), "Tester"));
-    private final ProductData product = new ProductData(Id.generate(), new Money(100), "test", ProductType.FOOD, new Date());
-    private final RequestItem item = new RequestItem(product, 10, new Money(100));
+    private final ProductData product = new ProductData(Id.generate(), new Money(100), "test", ProductType.FOOD,
+            new Date());
 
     @Before
     public void setUp() {
         policy = mock(TaxPolicy.class);
-        factory = mock(InvoiceFactory.class);
-        bookKeeper = new BookKeeper(factory);        
+        // factory = mock(InvoiceFactory.class);
+        bookKeeper = new BookKeeper(factory);
     }
 
     @Test
     public void testInvoiceShouldContainOneItem() {
+        final RequestItem item = new RequestItem(product, 10, new Money(100));
         request.add(item);
-        when(policy.calculateTax(ProductType.FOOD, item.getTotalCost())).thenReturn(new Tax(new Money(new BigDecimal(10)), "Def"));
+        when(policy.calculateTax(Matchers.any(ProductType.class), Matchers.any(Money.class)))
+                .thenReturn(new Tax(new Money(new BigDecimal(10)), "Def"));
         inv = bookKeeper.issuance(request, policy);
         Assert.assertThat(inv.getItems().size(), is(1));
     }
+
     @Test
     public void testInvoiceShouldCallTaxCalculationTwice() {
+        final RequestItem item = new RequestItem(product, 10, new Money(100));
         request.add(item);
-        when(policy.calculateTax(ProductType.FOOD, item.getTotalCost())).thenReturn(new Tax(new Money(new BigDecimal(10)), "Def"));
+        request.add(item);
+        when(policy.calculateTax(Matchers.any(ProductType.class), Matchers.any(Money.class)))
+                .thenReturn(new Tax(new Money(new BigDecimal(10)), "Def"));
         inv = bookKeeper.issuance(request, policy);
-        Assert.assertThat(inv.getItems().size(), is(1));
+        verify(policy, times(2)).calculateTax(Matchers.any(ProductType.class), Matchers.any(Money.class));
     }
 }
