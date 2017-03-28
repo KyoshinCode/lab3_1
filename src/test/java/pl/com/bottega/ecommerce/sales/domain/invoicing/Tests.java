95b6f8b5ec537d44e718530db5116ce1a0975d2f
patrykxk <patrix215@gmail.com>
2017-03-28 08:57:58 -0700

    Dokonczony 1 test faktura z jedna pozycja

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/Tests.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/Tests.java
@@ -3,39 +3,40 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 import org.junit.Test;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
-import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import java.util.Arrays;
 import java.util.Calendar;
-
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType.FOOD;
 
 public class Tests {
     @Test
     public void InvoiceRequestWithOneInvoice(){
-        ClientData clientData = new ClientData(Id.generate(), "Patryk");
-        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+        //given
+        ProductData productData = new ProductData(Id.generate(), new Money(20), "Ziemniaki", ProductType.FOOD, Calendar.getInstance().getTime());
+        RequestItem item = new RequestItem(productData,  10,  new Money(100));
 
-        ProductData productData = new ProductData(Id.generate(), new Money(20), "xy", FOOD, Calendar.getInstance().getTime());
-        Money totalCost = new Money(100);
-        RequestItem item = new RequestItem(productData,  10,  totalCost);
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+        BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
 
-        invoiceRequest.add(item);
-        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        InvoiceRequest mockInvoiceRequest = mock(InvoiceRequest.class);
+        TaxPolicy mockTaxPolicy = mock(TaxPolicy.class);
 
-        BookKeeper bookKeeper = mock(BookKeeper.class);
-        when(bookKeeper.issuance(invoiceRequest,taxPolicy).getItems().size());
-        Invoice invoice = bookKeeper.issuance(invoiceRequest,taxPolicy);
+        when(mockInvoiceRequest.getItems()).thenReturn(Arrays.asList(item));
+        when(mockTaxPolicy.calculateTax(ProductType.FOOD, item.getTotalCost())).thenReturn(new Tax(new Money(5),"Desc"));
 
-        int result = invoice.getItems().size();
-        assertThat(result, equalTo(1) );
-        //bookKeeper.issuance();
+        //when
+        Invoice invoice = bookKeeper.issuance(mockInvoiceRequest,mockTaxPolicy);
+
+        //then
+        assertThat(invoice.getItems().size(), equalTo(1) );
+
     }
 }
