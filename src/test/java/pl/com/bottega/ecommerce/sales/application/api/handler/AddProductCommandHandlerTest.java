665575bbb68403b19fa6e6e5e1d28f4dcdd0eb78
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-28 16:47:45 +0200

    pkt 1.2: utworzenie klasy testowej AddProductCommandHandlerTest

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,19 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddProductCommandHandlerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
