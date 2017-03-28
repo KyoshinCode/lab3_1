c0108ff4200f844148bf65c0a6be0875ffccec55
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-28 15:09:36 +0200

    Add stub implementations of AddProductCommandHandler fields into class object.

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -2,7 +2,13 @@ package pl.com.bottega.ecommerce.sales.application.api.handler;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.util.reflection.Whitebox;
 import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
 
 import static org.mockito.Mockito.mock;
 
@@ -13,12 +19,28 @@ public class AddProductCommandHandlerTest {
 
     private AddProductCommandHandler addProductCommandHandler;
     private AddProductCommand addProductCommand;
+    private ReservationRepository reservationRepository;
+    private ProductRepository productRepository;
+    private SuggestionService suggestionService;
+    private ClientRepository clientRepository;
+    private SystemContext systemContext;
 
     @Before
     public void init() {
         addProductCommandHandler = new AddProductCommandHandler();
 
         addProductCommand = mock(AddProductCommand.class);
+        reservationRepository = mock(ReservationRepository.class);
+        productRepository = mock(ProductRepository.class);
+        suggestionService = mock(SuggestionService.class);
+        clientRepository = mock(ClientRepository.class);
+        systemContext = mock(SystemContext.class);
+
+        Whitebox.setInternalState(addProductCommandHandler, "reservationRepository", reservationRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "productRepository", productRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "suggestionService", suggestionService);
+        Whitebox.setInternalState(addProductCommandHandler, "clientRepository", clientRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "systemContext", systemContext);
     }
 
     @Test
