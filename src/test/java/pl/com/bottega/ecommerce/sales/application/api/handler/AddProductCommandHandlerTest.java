abd99b33ba2206d12d66380f414f51e38e6a2715
Andrzej Borzecki <andrew.borzecki@gmail.com>
2017-03-29 02:07:34 +0200

    Added test case (check if reservation repository save method has been called on AddProductCommand),changed Reservation and Product constructor to public

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -1,8 +1,77 @@
 package pl.com.bottega.ecommerce.sales.application.api.handler;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.internal.util.reflection.Whitebox;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.Client;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+import pl.com.bottega.ecommerce.system.application.SystemUser;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by grusz on 25.03.2017.
  */
 public class AddProductCommandHandlerTest {
-    
+
+
+    private ProductRepository productRepository;
+    private ReservationRepository reservationRepository;
+    private SuggestionService suggestionService;
+    private Id id;
+    private Reservation reservation;
+    private AddProductCommand addProductCommand;
+    private AddProductCommandHandler addProductCommandHandler;
+
+    @Before
+    public void setUp() throws Exception {
+
+        productRepository = mock(ProductRepository.class);
+        reservationRepository = mock(ReservationRepository.class);
+        suggestionService = mock(SuggestionService.class);
+        ClientRepository clientRepository = mock(ClientRepository.class);
+        id = new Id("1");
+        addProductCommand = new AddProductCommand(id,id,1);
+        addProductCommandHandler = new AddProductCommandHandler();
+        ClientData clientData = new ClientData(Id.generate(), "Andrew");
+        Product product = new Product(id,new Money(100),"product", ProductType.FOOD);
+        reservation = new Reservation(id, Reservation.ReservationStatus.OPENED,clientData,new Date());
+        Client client = new Client();
+        SystemContext systemContext = new SystemContext();
+        when(clientRepository.load(id)).thenReturn(client);
+        when(reservationRepository.load(addProductCommand.getOrderId())).thenReturn(reservation);
+        when(productRepository.load(addProductCommand.getProductId())).thenReturn(product);
+        when(suggestionService.suggestEquivalent(product,client)).thenReturn(product);
+        Whitebox.setInternalState(addProductCommandHandler,"reservationRepository",reservationRepository);
+        Whitebox.setInternalState(addProductCommandHandler,"productRepository",productRepository);
+        Whitebox.setInternalState(addProductCommandHandler,"suggestionService",suggestionService);
+        Whitebox.setInternalState(addProductCommandHandler,"clientRepository",clientRepository);
+        Whitebox.setInternalState(addProductCommandHandler,"systemContext",systemContext);
+
+
+
+    }
+
+    @Test public void addProduct_checkIfReservationSaveMethodHasBeenCalled(){
+
+        addProductCommandHandler.handle(addProductCommand);
+        verify(reservationRepository).save(reservation);
+
+    }
 }
