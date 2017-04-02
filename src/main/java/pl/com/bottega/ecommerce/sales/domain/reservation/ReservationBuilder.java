5f48e2d5b243bd2d776fda4c2b8b7b88137ccdd4
Konrad Gos <goskonrad@outlook.com>
2017-04-02 19:33:23 +0200

    [builder] + ReservationBuilder class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/reservation/ReservationBuilder.java
@@ -0,0 +1,47 @@
+package pl.com.bottega.ecommerce.sales.domain.reservation;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+import java.util.Date;
+
+/**
+ * Created by Konrad Gos on 02.04.2017.
+ */
+public class ReservationBuilder {
+    private Id id = Id.generate();
+    private Reservation.ReservationStatus status;
+    private ClientData clientData;
+    private Date createDate;
+
+    public ReservationBuilder() {
+    }
+
+    public ReservationBuilder Reservation() {
+        return new ReservationBuilder();
+    }
+
+    public ReservationBuilder withId(Id id) {
+        this.id = id;
+        return this;
+    }
+
+    public ReservationBuilder withStatus(Reservation.ReservationStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    public ReservationBuilder withClientData(ClientData clientData) {
+        this.clientData = clientData;
+        return this;
+    }
+
+    public ReservationBuilder withCreateDate(Date createDate) {
+        this.createDate = createDate;
+        return this;
+    }
+
+    public Reservation build() {
+        return new Reservation(id,status, clientData, createDate);
+    }
+}
