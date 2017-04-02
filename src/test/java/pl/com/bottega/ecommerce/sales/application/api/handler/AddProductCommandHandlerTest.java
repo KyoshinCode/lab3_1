d086f4d8ff123052297294c36f2c7f4d164bb7df
Sasho <Sasho>
2017-04-02 17:09:55 +0200

    Dodano pierwsza metode testowa - czy nie jest wywowlywana metoda clientLoad() przy dostepnym produkcie

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -7,14 +7,23 @@ import org.mockito.Mock;
 import org.mockito.internal.util.reflection.Whitebox;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
 import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
 import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 import pl.com.bottega.ecommerce.system.application.SystemContext;
 
+import java.util.Date;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Sasho on 2017-04-02.
@@ -38,6 +47,8 @@ public class AddProductCommandHandlerTest {
 
     private AddProductCommandHandler handler;
 
+    private AddProductCommand command;
+
     @Before
     public void setUp() throws Exception {
         handler = new AddProductCommandHandler();
@@ -47,6 +58,19 @@ public class AddProductCommandHandlerTest {
         Whitebox.setInternalState(handler, "suggestionService", suggestionService);
         Whitebox.setInternalState(handler, "clientRepository", clientRepository);
         Whitebox.setInternalState(handler, "systemContext", systemContext);
+        command = new AddProductCommand(Id.generate(), Id.generate(), 1);
     }
 
+    @Test
+    public void testHandler_productAvailable_NoClientLoad() throws Exception {
+        Reservation reservation = new Reservation(Id.generate(), Reservation.ReservationStatus.OPENED, new ClientData(Id.generate(), "Pusia"), new Date());
+        Product product = new Product(Id.generate(), new Money(10), "ciasto z buraka", ProductType.FOOD);
+
+        when(reservationRepository.load(command.getOrderId())).thenReturn(reservation);
+        when(productRepository.load(command.getProductId())).thenReturn(product);
+
+        handler.handle(command);
+
+        verify(clientRepository, never()).load(reservation.getClientData().getAggregateId());
+    }
 }
