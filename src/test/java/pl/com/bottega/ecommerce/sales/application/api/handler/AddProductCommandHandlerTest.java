ff4078b82fb631c1447fd2660dd33b538cabcc55
Piotr Dudek <194936@edu.p.lodz.pl>
2017-04-02 16:28:41 +0200

    setUp before i reservationSaveMethodCancelTest -> DODANE

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -1,5 +1,25 @@
 package pl.com.bottega.ecommerce.sales.application.api.handler;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.internal.util.reflection.Whitebox;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.Client;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +27,45 @@ import static org.junit.Assert.*;
  */
 public class AddProductCommandHandlerTest {
 
+    private ReservationRepository reservationRepository;
+    private ProductRepository productRepository;
+    private SuggestionService suggestionService;
+    private Reservation reservation;
+    private Id id;
+    private AddProductCommand addProductCommand;
+    private AddProductCommandHandler addProductCommandHandler;
+
+    @Before
+    public void setUp() throws Exception{
+        reservationRepository = Mockito.mock(ReservationRepository.class);
+        productRepository = Mockito.mock(ProductRepository.class);
+        suggestionService = Mockito.mock(SuggestionService.class);
+        ClientRepository clientRepository = Mockito.mock(ClientRepository.class);
+        id = new Id("1");
+        addProductCommand = new AddProductCommand(id, id, 1);
+        addProductCommandHandler = new AddProductCommandHandler();
+        ClientData clientData = new ClientData(Id.generate(), "TEST");
+        Product product = new Product(id, new Money(200), "test", ProductType.STANDARD);
+        reservation = new Reservation(id, Reservation.ReservationStatus.OPENED, clientData, new Date());
+        Client client = new Client();
+        SystemContext systemContext = new SystemContext();
+
+        Mockito.when(clientRepository.load(id)).thenReturn(client);
+        Mockito.when(reservationRepository.load(addProductCommand.getOrderId())).thenReturn(reservation);
+        Mockito.when(productRepository.load(addProductCommand.getProductId())).thenReturn(product);
+        Mockito.when(suggestionService.suggestEquivalent(product, client)).thenReturn(product);
+
+        //static field set
+        Whitebox.setInternalState(addProductCommandHandler, "reservationRepository", reservationRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "productRepository", productRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "suggestionService", suggestionService);
+        Whitebox.setInternalState(addProductCommandHandler, "clientRepository", clientRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "systemContext", systemContext);
+    }
+
+    @Test
+    public void reservationSaveMethodCancelTest(){
+        addProductCommandHandler.handle(addProductCommand);
+        Mockito.verify(reservationRepository).save(reservation);
+    }
 }
