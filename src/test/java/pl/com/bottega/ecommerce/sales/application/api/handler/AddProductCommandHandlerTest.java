df0ff414092fe59ab8a0e68240a34b181be498fd
Piotr Dudek <194936@edu.p.lodz.pl>
2017-04-02 16:07:14 +0200

    Product i Reservation -> konstuktory publiczne

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,10 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Piotrek on 02.04.2017.
+ */
+public class AddProductCommandHandlerTest {
+
+}
