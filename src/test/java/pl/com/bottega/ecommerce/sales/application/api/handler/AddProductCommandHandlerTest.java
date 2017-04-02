3ac01f2e703c82ac956b3e117d93c22a65b16ae1
Aleksandra Dura <Aleksandra.Dura@sdqac.rd.tp.pl>
2017-04-02 22:24:22 +0200

    created AddProductCommandHandlerTest and added setUp

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,43 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.internal.util.reflection.Whitebox;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+
+import static org.mockito.Mockito.mock;
+
+
+public class AddProductCommandHandlerTest {
+    private ReservationRepository reservationRepository;
+    private ProductRepository productRepository;
+    private SuggestionService suggestionService;
+    private ClientRepository clientRepository;
+    private AddProductCommandHandler addProductCommandHandler;
+    private SystemContext systemContext;
+    @Before
+    public void setUp() throws Exception {
+        reservationRepository = mock(ReservationRepository.class);
+        productRepository = mock(ProductRepository.class);
+        suggestionService = mock(SuggestionService.class);
+        clientRepository = mock(ClientRepository.class);
+        addProductCommandHandler = new AddProductCommandHandler();
+        systemContext = new SystemContext();
+
+        Whitebox.setInternalState(addProductCommandHandler, "reservationRepository", reservationRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "productRepository", productRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "suggestionService", suggestionService);
+        Whitebox.setInternalState(addProductCommandHandler, "clientRepository", clientRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "systemContext", systemContext);
+    }
+
+    @Test
+    public void handle() throws Exception {
+
+    }
+
+}
