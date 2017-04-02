bc2cb8df2227d9a86f489e77c38f81e4f13502e4
Konrad Gos <goskonrad@outlook.com>
2017-04-02 13:47:20 +0200

    + test class for AddProductCommandHandler class

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import org.junit.Before;
+
+/**
+ * Created by Konrad Gos on 02.04.2017.
+ */
+public class AddProductCommandHandlerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+}
