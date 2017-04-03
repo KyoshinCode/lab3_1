dd1b4f67a7255ca54b325dae4bd2d4dbbb9fc4de
klaglu <klaudia.gluszek@gmail.com>
2017-04-03 23:19:49 +0200

    builder dadanie ProductBuilder

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductBuilder.java
@@ -0,0 +1,41 @@
+package pl.com.bottega.ecommerce.sales.domain.productscatalog;
+
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class ProductBuilder {
+    private Money price = new Money(0);
+    private String name = "name";
+    private ProductType productType = ProductType.STANDARD;
+    private Id id = Id.generate();
+
+    public ProductBuilder withPrice(Money price) {
+        this.price = price;
+        return this;
+    }
+
+    public ProductBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ProductBuilder withProductType(ProductType productType) {
+        this.productType = productType;
+        return this;
+    }
+
+    public ProductBuilder withId(Id id) {
+        this.id = id;
+        return this;
+    }
+
+    static ProductBuilder product() {
+        return new ProductBuilder();
+    }
+
+    public Product build() {
+        return new Product(id, price, name, productType);
+    }
+
+}
