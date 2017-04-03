14022592f9811f0d20a7cc344ed323ea38014fc9
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-04-03 23:51:39 +0200

    isolation - aktualizacja testInvoiceOnePositionReturnOne

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,32 +1,37 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import static org.junit.Assert.*;
-import org.hamcrest.Matchers;
 
 import org.junit.Test;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
+
+import java.util.Date;
 
 public class BookKeeperTest {
 
 	@Test
 	public void testInvoiceOnePositionReturnOne() {
+		InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(), "Kuba"));
+
+		BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+
+		TaxPolicy taxPolicy = mock(TaxPolicy.class);
+		when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(
+				new Tax(new Money(1), "EUR"));
 		
-		BookKeeper bookKeeper = mock(BookKeeper.class);
-		//Invoice mockedInvoice = mock(Invoice.class);
-		//mockedInvoice.addItem(item);
-		TaxPolicy tax = mock(TaxPolicy.class);
-		RequestItem itemRequest = mock(RequestItem.class);
-		ClientData clientData = mock(ClientData.class);
-		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
-		
-		invoiceRequest.add(itemRequest);
-		
-		bookKeeper.issuance(invoiceRequest, tax);
-		
-		assertThat(invoiceRequest.getItems().size(), Matchers.is(1));
+		RequestItem item = new RequestItem
+				(new ProductData(Id.generate(), new Money(200), "Banan", ProductType.FOOD, new Date()), 1, new Money(20));
+
+		invoiceRequest.add(item);
 	}
 
 }
