66a9561613ad6fdc7be760356a6ff80e81cd5e69
Patryk Wierzyński <p.wierzynski@live.com>
2017-04-04 01:52:08 +0200

    builder: Added handle_WhenProductUnavailableSuggestionServiceAndClientRepositoryCalledOnce test case

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.mockito.internal.util.reflection.Whitebox;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.Client;
 import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
 import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
@@ -17,8 +18,7 @@ import pl.com.bottega.ecommerce.system.application.SystemContext;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Patryk Wierzyński.
@@ -34,13 +34,16 @@ public class AddProductCommandHandlerTest {
 	private ClientRepository clientRepository;
 	private SystemContext systemContext;
 
+	private Reservation reservation;
+	private Product product;
+
 	@Before
 	public void setUp() throws Exception {
 		reservationRepository = mock(ReservationRepository.class);
 		productRepository = mock(ProductRepository.class);
 		suggestionService = mock(SuggestionService.class);
 		clientRepository = mock(ClientRepository.class);
-		systemContext = mock(SystemContext.class);
+		systemContext = new SystemContext();
 
 		handler = new AddProductCommandHandler();
 		Whitebox.setInternalState(handler, "reservationRepository", reservationRepository);
@@ -50,19 +53,36 @@ public class AddProductCommandHandlerTest {
 		Whitebox.setInternalState(handler, "systemContext", systemContext);
 
 		command = new AddProductCommand(Id.generate(), Id.generate(), 12);
-	}
+		reservation = new ReservationBuilder().opened().build();
+		product = new ProductBuilder().withId(command.getProductId()).build();
 
-	@Test
-	public void handle_ProductAddedToReservationIsCorrect() throws Exception {
-		Reservation reservation = new ReservationBuilder().opened().build();
-		Product product = new ProductBuilder().withId(command.getProductId()).build();
 
 		when(reservationRepository.load(command.getOrderId())).thenReturn(reservation);
 		when(productRepository.load(command.getProductId())).thenReturn(product);
 
+	}
+
+	@Test
+	public void handle_ProductAddedToReservationIsCorrect() throws Exception {
 		handler.handle(command);
 
 		assertThat(reservation.contains(product), is(true));
+	}
+
+	@Test
+	public void handle_WhenProductUnavailableSuggestionServiceAndClientRepositoryCalledOnce() throws Exception {
+		product.markAsRemoved();
+
+		Client client = new Client();
+		Product suggested = new ProductBuilder().withName("suggestedDefault").build();
+
+		when(clientRepository.load(systemContext.getSystemUser().getClientId())).thenReturn(client);
+		when(suggestionService.suggestEquivalent(product, client)).thenReturn(suggested);
+
+		handler.handle(command);
+
+		verify(suggestionService, times(1)).suggestEquivalent(product, client);
+		verify(clientRepository, times(1)).load(systemContext.getSystemUser().getClientId());
 	}
 
 }
