c56ede461b19bc5dddb59adc130d787f0bfafc38
klaglu <klaudia.gluszek@gmail.com>
2017-04-03 22:29:20 +0200

    Dodanie testu sprawdzającego akcje zastępowania niedostępnego produktu

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -2,8 +2,6 @@ package pl.com.bottega.ecommerce.sales.application.api.handler;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.internal.util.reflection.Whitebox;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
@@ -17,10 +15,13 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
 
 import java.util.Date;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
@@ -33,23 +34,32 @@ public class AddProductCommandHandlerTest {
     private ClientRepository clientRepository;
     private SuggestionService suggestionService;
     private Product product;
-
+    private ReservationRepository reservationRepository;
+    private SystemContext systemContext;
+    private Product equivalentProduct;
+    private Client client = new Client();
+    private Id id = new Id("1");
     @Before
     public void setUp() throws Exception {
-        command = new AddProductCommand(Id.generate(), Id.generate(), 1);
-        reservation  = new Reservation(Id.generate(), Reservation.ReservationStatus.OPENED,new ClientData(Id.generate(), "name"),new Date());
+        command = new AddProductCommand(id, id, 1);
+        reservation = spy(new Reservation(Id.generate(), Reservation.ReservationStatus.OPENED, new ClientData(Id.generate(), "name"), new Date()));
         suggestionService = mock(SuggestionService.class);
         productRepository = mock(ProductRepository.class);
-        clientRepository= mock(ClientRepository.class);
-        product = new Product(Id.generate(), new Money(10),"Test name", ProductType.FOOD);
+        clientRepository = mock(ClientRepository.class);
+        systemContext = new SystemContext();
+        product = new Product(Id.generate(), new Money(10), "Test name", ProductType.FOOD);
+        equivalentProduct  = new Product(Id.generate(), new Money(100), "eqproduct", ProductType.FOOD);
         addProductCommandHandler = new AddProductCommandHandler();
+        reservationRepository = mock(ReservationRepository.class);
+        Whitebox.setInternalState(addProductCommandHandler, "reservationRepository", reservationRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "productRepository", productRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "suggestionService", suggestionService);
+        Whitebox.setInternalState(addProductCommandHandler, "clientRepository", clientRepository);
+        Whitebox.setInternalState(addProductCommandHandler, "systemContext", systemContext);
     }
 
     @Test
     public void testHandleSaveCallOne() throws Exception {
-        ReservationRepository reservationRepository = mock(ReservationRepository.class);
-        Whitebox.setInternalState(addProductCommandHandler, "reservationRepository", reservationRepository);
-        Whitebox.setInternalState(addProductCommandHandler, "productRepository",productRepository);
         when(productRepository.load(any(Id.class))).thenReturn(product);
         when(reservationRepository.load(any(Id.class))).thenReturn(reservation);
 
@@ -58,5 +68,18 @@ public class AddProductCommandHandlerTest {
         verify(reservationRepository, times(1)).save(any(Reservation.class));
 
     }
+
+    @Test
+    public void testHandleSugestionEquivalentWhenProductNotAvaiable() {
+        when(clientRepository.load(id)).thenReturn(client);
+        when(reservationRepository.load(command.getOrderId())).thenReturn(reservation);
+        when(productRepository.load(command.getProductId())).thenReturn(product);
+        when(suggestionService.suggestEquivalent(product, client)).thenReturn(equivalentProduct);
+        product.markAsRemoved();
+        assertThat(product.isAvailable(), is(equalTo(false)));
+        addProductCommandHandler.handle(command);
+        verify(reservation).add(equivalentProduct, 1);
+    }
+
 
 }
