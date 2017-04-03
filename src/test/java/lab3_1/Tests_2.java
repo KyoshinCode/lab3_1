a0daabc827a467bf43547b29b8260d8f873bece7
Patron <patroneq@gmail.com>
2017-04-03 21:11:43 +0200

    builder - zmiany w ProductBuilder

--- src/test/java/lab3_1/Tests_2.java
+++ src/test/java/lab3_1/Tests_2.java
@@ -6,15 +6,23 @@ package lab3_1;
  import org.junit.Test;
  import org.mockito.Mockito;
  import org.mockito.internal.util.reflection.Whitebox;
+ import static org.hamcrest.Matchers.*;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
 import pl.com.bottega.ecommerce.sales.application.api.handler.AddProductCommandHandler;
  import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
  import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
- import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
- import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
- import pl.com.bottega.ecommerce.system.application.SystemContext;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductBuilder;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationBuilder;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
  
  public class Tests_2 {
  	
@@ -41,7 +49,22 @@ import pl.com.bottega.ecommerce.sales.application.api.handler.AddProductCommandH
  	}
  	
  	@Test
- 	public void test() {
- 		fail("Not yet implemented");
- 	}
+ 	public void testAvailableProductCorrectlyAddedToReservation() {
+ 	Product tProd = new ProductBuilder()
+		.available()
+		.withName("test product")
+		.withPrice(new Money(123))
+		.withType(ProductType.STANDARD)
+		.withId(Id.generate())
+		.build();
+	Reservation tRes = new ReservationBuilder()
+		.open()
+		.withClientData(new ClientData(Id.generate(), "wujeksado"))
+		.withId(Id.generate())
+		.build();
+	Mockito.when(this.prRep.load(testCommand.getProductId())).thenReturn(tProd);
+	Mockito.when(this.resRep.load(testCommand.getOrderId())).thenReturn(tRes);
+	testHandler.handle(testCommand);
+	assertThat(tRes.contains(tProd), is(equalTo(true)));
+  	}
  }
