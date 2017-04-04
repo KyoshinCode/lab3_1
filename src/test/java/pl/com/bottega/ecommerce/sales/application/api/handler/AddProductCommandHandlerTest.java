726085b268037357b0a089ac612d0d8efee59106
patrykxk <patrix215@gmail.com>
2017-04-04 12:20:19 +0200

    Test czy produkt jest dodawany do rezerwacji

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -1,7 +1,82 @@
 package pl.com.bottega.ecommerce.sales.application.api.handler;
 
-/**
- * Created by Patryk on 2017-04-02.
- */
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
+
+import org.mockito.internal.util.reflection.Whitebox;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+
+import java.util.Date;
+
+
 public class AddProductCommandHandlerTest {
+
+    private AddProductCommandHandler handler;
+    private ReservationRepository reservationRepository;
+    private ProductRepository productRepository;
+    private SuggestionService suggestionService;
+    private ClientRepository clientRepository;
+    private SystemContext systemContext;
+
+    @Before
+    public void setUp(){
+        handler = new AddProductCommandHandler();
+
+        reservationRepository = mock(ReservationRepository.class);
+        productRepository = mock(ProductRepository.class);
+        suggestionService = mock(SuggestionService.class);
+        clientRepository = mock(ClientRepository.class);
+        systemContext = mock(SystemContext.class);
+
+
+
+        Whitebox.setInternalState(handler, "reservationRepository", reservationRepository);
+        Whitebox.setInternalState(handler, "productRepository", productRepository);
+        Whitebox.setInternalState(handler, "suggestionService", suggestionService);
+        Whitebox.setInternalState(handler, "clientRepository", clientRepository);
+        Whitebox.setInternalState(handler, "systemContext", systemContext);
+    }
+
+
+
+    @Test
+    public void isReservationSaved(){
+        AddProductCommand command = new AddProductCommand(Id.generate(),Id.generate(),5);
+        Reservation reservation = new Reservation(Id.generate(), Reservation.ReservationStatus.OPENED, new ClientData(Id.generate(), "JAN"), new Date());
+        Product product = new Product(Id.generate(),new Money(100),"product", ProductType.FOOD);
+
+        when(reservationRepository.load(any(Id.class))).thenReturn(reservation);
+        when(productRepository.load(any(Id.class))).thenReturn(product);
+
+        handler.handle(command);
+        verify(reservationRepository).save(reservation);
+    }
+
+    @Test
+    public void isProductAdded(){
+        AddProductCommand command = new AddProductCommand(Id.generate(),Id.generate(),5);
+        Reservation reservation = new Reservation(Id.generate(), Reservation.ReservationStatus.OPENED, new ClientData(Id.generate(), "JAN"), new Date());
+        Product product = new Product(Id.generate(),new Money(100),"product", ProductType.FOOD);
+
+        when(reservationRepository.load(any(Id.class))).thenReturn(reservation);
+        when(productRepository.load(any(Id.class))).thenReturn(product);
+        handler.handle(command);
+
+        assertFalse(reservation.getReservedProducts().isEmpty());
+    }
 }
