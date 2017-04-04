bc41542b16a993dbf2cb74987d6ce8c655ce58a1
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-04-04 14:04:31 +0200

    Product private->public, ProductBuilder added

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -5,9 +5,13 @@ import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
 import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
 import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
 import pl.com.bottega.ecommerce.system.application.SystemContext;
 
@@ -18,6 +22,10 @@ public class AddProductCommandHandlerTest {
 	private SuggestionService suggestionService;
 	private ClientRepository clientRepository;
 	private SystemContext systemContext;
+	private AddProductCommand addProductCommand;
+	private Reservation reservation;
+	private Product product;
+	private AddProductCommandHandler addProductCommandHandler;
 
 	
 	@Test
@@ -28,7 +36,11 @@ public class AddProductCommandHandlerTest {
 		 clientRepository = mock(ClientRepository.class);
 		 systemContext = mock(SystemContext.class);
 		 		
-    
+		addProductCommandHandler = new AddProductCommandHandler();
+        addProductCommandHandler = new AddProductCommandHandler();
+        addProductCommand = new AddProductCommand(Id.generate(), Id.generate(), 1);
+        
+
 	}
 
 }
