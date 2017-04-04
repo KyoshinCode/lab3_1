2dd93690ac948e9414fd7e81fe0441e6820a4d1b
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-04-04 13:25:33 +0200

    isolation setUp refactorization

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -21,21 +21,27 @@ public class BookKeeperTest {
 	private ClientData clientData = new ClientData(Id.generate(), "Jan");
 	private InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
 	private ProductData productData = new ProductData(Id.generate(), new Money(100), "Apple", ProductType.FOOD, new Date(0));
+	private InvoiceFactory invoiceFcatory;
 	private BookKeeper bookKeeper;
 	private TaxPolicy taxPolicyMocked;
 	
 	
 	@Before
-	public void setUp(){
-		InvoiceFactory invoiceFcatory = new InvoiceFactory();
-		bookKeeper = new BookKeeper(invoiceFcatory);
-		TaxPolicy taxPolicyMocked = mock(TaxPolicy.class);
-		
-		when(taxPolicyMocked.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(100), "EUR"));
+	public void setUp() {
+		invoiceFcatory = new InvoiceFactory();
+		 bookKeeper = new BookKeeper(invoiceFcatory);
+		 taxPolicyMocked = mock(TaxPolicy.class);
+		 when(taxPolicyMocked.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(100), "EUR"));
 	}
+	
 	
 	@Test
 	public void invoiceWithOneItemTest() {
+		
+//		TaxPolicy taxPolicyMocked = mock(TaxPolicy.class);
+		
+//		when(taxPolicyMocked.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(100), "EUR"));
+		
 		RequestItem requestItem = new RequestItem(productData, 1, new Money(100));
 		invoiceRequest.add(requestItem);
 		
