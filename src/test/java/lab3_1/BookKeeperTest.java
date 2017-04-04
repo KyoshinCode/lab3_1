cf0a2a22116407a7a0781b9852cc0475d8ef1847
ciupa.kamil <194928@edu.p.lodz.pl>
2017-04-04 11:39:36 +0200

    Dodano metode setUp ; refaktoryzacja

--- src/test/java/lab3_1/BookKeeperTest.java
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -4,12 +4,13 @@ package lab3_1;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 
-import java.util.Arrays;
+
 import java.util.Date;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
@@ -26,75 +27,64 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeperTest {
+	
+	BookKeeper bookKeeper;
+	TaxPolicy mockedTaxPolicy;
+	InvoiceRequest invoiceRequest;
+	ProductData productData;
+	ProductData productData2;
+	
+	@Before
+	public void setUp(){
+		bookKeeper = new BookKeeper(new InvoiceFactory());
+		mockedTaxPolicy = mock(TaxPolicy.class);
+		invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(), "John Doe"));
+		productData = new ProductData(Id.generate(), new Money(10), "item", ProductType.DRUG, new Date());
+		productData2 = new ProductData(Id.generate(), new Money(10), "item2", ProductType.FOOD, new Date());
+	}
 
 	@Test
 	public void testInvoiceRequestWithOneItemReturnInvoiceWithOneItem() {
-		
-		//Given
-		BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-		TaxPolicy mockedTaxPolicy = mock(TaxPolicy.class);
-		InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(), "John Doe"));
-		ProductData productData = new ProductData(Id.generate(), new Money(10), "item", ProductType.DRUG, new Date());
 		
 		when(mockedTaxPolicy.calculateTax(productData.getType(), productData.getPrice())).thenReturn(new Tax(new Money(5), "Fake"));
 		 
 		RequestItem requestItem = new RequestItem(productData, 5, productData.getPrice());
 		invoiceRequest.add(requestItem);
 		
-		//When
 		Invoice invoice = bookKeeper.issuance(invoiceRequest, mockedTaxPolicy);
-		//Then
 		
 		Assert.assertThat(invoice.getItems().size(), is(equalTo(1)));
-		
 	}
 	
 	
 	@Test
 	public void testInvoiceWithTwoItemsUseCalculateTaxMethodTwoTimes() {
 		
-		//Given
-		BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-		TaxPolicy mockedTaxPolicy = mock(TaxPolicy.class);
-		InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(), "John Doe"));
-		ProductData productData = new ProductData(Id.generate(), new Money(10), "item", ProductType.DRUG, new Date());
-		
 		when(mockedTaxPolicy.calculateTax(productData.getType(), productData.getPrice())).thenReturn(new Tax(new Money(5), "Fake"));
 		 
 		RequestItem requestItem = new RequestItem(productData, 5, productData.getPrice());
 		invoiceRequest.add(requestItem);
 		invoiceRequest.add(requestItem);
 		
-		//When
-		Invoice invoice = bookKeeper.issuance(invoiceRequest, mockedTaxPolicy);
+		bookKeeper.issuance(invoiceRequest, mockedTaxPolicy);
 		
-		//Then
 		verify(mockedTaxPolicy, times(2)).calculateTax(productData.getType(), productData.getPrice());
 	}
 	
 	@Test
 	public void testInvoiceRequestWithTwoItemsReturnInvoiceWithTwoDifferentItems() {
 		
-		//Given
-				BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-				TaxPolicy mockedTaxPolicy = mock(TaxPolicy.class);
-				InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(), "John Doe"));
-				ProductData productData = new ProductData(Id.generate(), new Money(10), "item", ProductType.DRUG, new Date());
-				ProductData productData2 = new ProductData(Id.generate(), new Money(10), "item2", ProductType.FOOD, new Date());
-				when(mockedTaxPolicy.calculateTax(productData.getType(), productData.getPrice())).thenReturn(new Tax(new Money(5), "Fake"));
-				when(mockedTaxPolicy.calculateTax(productData2.getType(), productData2.getPrice())).thenReturn(new Tax(new Money(5), "Fake"));
-				 
-				RequestItem requestItem = new RequestItem(productData, 5, productData.getPrice());
-				invoiceRequest.add(requestItem);
-				requestItem = new RequestItem(productData2, 4, productData2.getPrice());
-				invoiceRequest.add(requestItem);
+		when(mockedTaxPolicy.calculateTax(productData.getType(), productData.getPrice())).thenReturn(new Tax(new Money(5), "Fake"));
+		when(mockedTaxPolicy.calculateTax(productData2.getType(), productData2.getPrice())).thenReturn(new Tax(new Money(5), "Fake"));
+		 
+		RequestItem requestItem = new RequestItem(productData, 5, productData.getPrice());
+		invoiceRequest.add(requestItem);
+		requestItem = new RequestItem(productData2, 4, productData2.getPrice());
+		invoiceRequest.add(requestItem);
 				
-				//When
-				Invoice invoice = bookKeeper.issuance(invoiceRequest, mockedTaxPolicy);
+		Invoice invoice = bookKeeper.issuance(invoiceRequest, mockedTaxPolicy);
 		
-				//Then
-				Assert.assertThat(invoice.getItems().get(0).getProduct(), is(not(invoice.getItems().get(1).getProduct())));
-		
+		Assert.assertThat(invoice.getItems().get(0).getProduct(), is(not(invoice.getItems().get(1).getProduct())));
 	}
 	
 	
