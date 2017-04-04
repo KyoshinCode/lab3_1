ce47ba226524396df7cca45be531a2facb4ac177
Szyrner.Milosz <195069@edu.p.lodz.pl>
2017-04-04 11:27:24 +0200

    builder: Dodanie kontruktora, zestawu metod oraz metody build()

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceLineBuilder.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceLineBuilder.java
@@ -9,5 +9,30 @@ public class InvoiceLineBuilder {
 	private int quantity;
 	private Money net;
 	private Tax tax;
-
+	
+	public InvoiceLineBuilder() {}
+	
+	public InvoiceLineBuilder withProductData(ProductData product) {
+		this.product = product;
+		return this;
+	}
+	
+	public InvoiceLineBuilder withQuantity(int quantity) {
+		this.quantity = quantity;
+		return this;
+	}
+	
+	public InvoiceLineBuilder withNet(Money net) {
+		this.net = net;
+		return this;
+	}
+	
+	public InvoiceLineBuilder withTax(Tax tax) {
+		this.tax = tax;
+		return this;
+	}
+	
+	public InvoiceLine build() {
+		return new InvoiceLine(product, quantity, net, tax);
+	}
 }
