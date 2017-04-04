5cc05b7123833604bdd8c3e074afc1baf24fda78
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-04-04 13:25:09 +0200

    isolation Dodanie importow w klasei BookKeeperTest

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -4,6 +4,14 @@ import org.junit.Test;
 import org.junit.Before;
 import org.mockito.Mock;
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.*;
+import pl.com.bottega.ecommerce.sales.domain.client.*;
+import pl.com.bottega.ecommerce.sales.domain.payment.*;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 
 public class BookKeeperTest {
