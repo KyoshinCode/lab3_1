46b1ae3f58ce70835fe4a2470d97f086b2bc7dd3
ciupa.kamil <194928@edu.p.lodz.pl>
2017-04-04 10:53:01 +0200

    Dodano test zwracajacy fakture z jedna pozycja

--- src/test/java/lab3_1/BookKeeperTest.java
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -1,5 +1,52 @@
 package lab3_1;
 
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.BookKeeper;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceRequest;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.RequestItem;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Tax;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.TaxPolicy;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 public class BookKeeperTest {
 
+	@Test
+	public void invoiceWithOneItem() {
+		
+		//Given
+		BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+		TaxPolicy mockedTaxPolicy = mock(TaxPolicy.class);
+		InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(), "John Doe"));
+		ProductData productData = new ProductData(Id.generate(), new Money(10), "item", ProductType.DRUG, new Date());
+		
+		when(mockedTaxPolicy.calculateTax(productData.getType(), productData.getPrice())).thenReturn(new Tax(new Money(5), "Fake"));
+		 
+		RequestItem requestItem = new RequestItem(productData, 5, productData.getPrice());
+		invoiceRequest.add(requestItem);
+		
+		//When
+		Invoice invoice = bookKeeper.issuance(invoiceRequest, mockedTaxPolicy);
+		//Then
+		
+		Assert.assertThat(invoice.getItems().size(), is(equalTo(1)));
+		
+	}
+	
 }
