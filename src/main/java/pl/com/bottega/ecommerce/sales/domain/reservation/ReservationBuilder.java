3b6cc75f723801de117d5b824c64a862f5d8274d
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-04-04 12:18:44 +0200

    [builder] ReservationBuilder - utworzenie potrzebnych metod

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/reservation/ReservationBuilder.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/reservation/ReservationBuilder.java
@@ -15,4 +15,28 @@ public class ReservationBuilder {
 	
 	public ReservationBuilder() {
 	}
+	
+	public ReservationBuilder withAggregateId(Id aggregateId) {
+		this.aggregateId = aggregateId;
+		return this;
+	}
+	
+	public ReservationBuilder withStatus(ReservationStatus status) {
+		this.status = status;
+		return this;
+	}
+	
+	public ReservationBuilder withClientData(ClientData clientData) {
+		this.clientData = clientData;
+		return this;
+	}
+	
+	public ReservationBuilder withDate(Date createDate) {
+		this.createDate = createDate;
+		return this;
+	}
+	
+	public Reservation build() {
+		return new Reservation(this.aggregateId, this.status, this.clientData, this.createDate);
+	}
 }
