af394669759e8be3f12f33a5370d7d8baa9b3cfc
Szyrner.Milosz <195069@edu.p.lodz.pl>
2017-04-04 11:25:15 +0200

    builder: Dodanie atrybutów

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceLineBuilder.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceLineBuilder.java
@@ -1,5 +1,13 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 public class InvoiceLineBuilder {
+	
+	private ProductData product;
+	private int quantity;
+	private Money net;
+	private Tax tax;
 
 }
