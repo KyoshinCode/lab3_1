5d0cf885720b4b9e8b40f53cd799d86c803ab957
Szyrner.Milosz <195069@edu.p.lodz.pl>
2017-04-04 11:06:03 +0200

    builder: Utworzenie RequestItemBuilder

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductDataBuilder.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductDataBuilder.java
@@ -13,4 +13,35 @@ public class ProductDataBuilder {
 	private Date snapshotDate = new Date();
 	private ProductType type = ProductType.STANDARD;
 	
+	public ProductDataBuilder() {}
+	
+	public ProductDataBuilder withId(Id productId) {
+		this.productId = productId;
+		return this;
+	}
+	
+	public ProductDataBuilder withPrice(Money price) {
+		this.price = price;
+		return this;
+	}
+	
+	public ProductDataBuilder withName(String name) {
+		this.name = name;
+		return this;
+	}
+	
+	public ProductDataBuilder withSnapshotDate(Date snapshotDate) {
+		this.snapshotDate = snapshotDate;
+		return this;
+	}
+	
+	public ProductDataBuilder withType(ProductType type) {
+		this.type = type;
+		return this;
+	}
+	
+	public ProductData build() {
+		return new ProductData(productId, price, name, type, snapshotDate);
+	}
+	
 }
