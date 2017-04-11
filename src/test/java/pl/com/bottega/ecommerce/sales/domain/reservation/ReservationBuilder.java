19559ceb56648fabc3a5533bfb32802eada9f835
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-04-11 14:09:26 +0200

    builder Add fields into ReservationBuilder.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/reservation/ReservationBuilder.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/reservation/ReservationBuilder.java
@@ -1,7 +1,22 @@
 package pl.com.bottega.ecommerce.sales.domain.reservation;
 
+import pl.com.bottega.ddd.support.domain.BaseAggregateRoot;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by Wojciech Szczepaniak on 11.04.2017.
  */
 public class ReservationBuilder {
+
+    private Id id = Id.generate();
+    private boolean removed = false;
+    private Reservation.ReservationStatus status = Reservation.ReservationStatus.OPENED;
+    private List<ReservationItem> items = new ArrayList<>();
+    private ClientData clientData = new ClientData(Id.generate(), "Test");
+    private Date createDate;
 }
