4865447adc1251b23b216f55c5cc05c87729660e
Adam Rezner <adam.rezner95@gmail.com>
2017-04-11 11:08:13 +0200

    AddProductCommandHandler attributes added

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerUnitTests.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerUnitTests.java
@@ -4,11 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+
 public class AddProductCommandHandlerUnitTests {
+	 
+ 	private ReservationRepository reservationRepository;
+ 	private ProductRepository productRepository;
+ 	private SuggestionService suggestionService;
+ 	private ClientRepository clientRepository;
+ 	private SystemContext systemContext;
+ 	private AddProductCommandHandler addProductCommandHandler;
+ 	private AddProductCommand command;
+ 	private Reservation reservation;
+ 	private Product product;
+ 	
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
-}
+  }
