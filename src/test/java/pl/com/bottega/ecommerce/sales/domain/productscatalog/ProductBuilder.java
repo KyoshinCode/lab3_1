ebfea9f7cc2679440a8bd3d26f2d72b4556b082f
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-04-11 11:30:53 +0200

    builder Add default values.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductBuilder.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductBuilder.java
@@ -1,7 +1,17 @@
 package pl.com.bottega.ecommerce.sales.domain.productscatalog;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 /**
  * Created by Wojciech Szczepaniak on 11.04.2017.
  */
 public class ProductBuilder {
+
+    private Id id = Id.generate();
+    private Money price = Money.ZERO;
+    private String name = "Test";
+    private ProductType productType = ProductType.STANDARD;
+
+
 }
