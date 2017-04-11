a224e0bdb575f658ac33c179f5681d6e1dc70924
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-04-11 11:38:29 +0200

    builder ProductBuilder - implements other required methods.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductBuilder.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductBuilder.java
@@ -13,5 +13,31 @@ public class ProductBuilder {
     private String name = "Test";
     private ProductType productType = ProductType.STANDARD;
 
+    public ProductBuilder withId(Id id) {
+        this.id = id;
 
+        return this;
+    }
+
+    public ProductBuilder withPrice(Money price) {
+        this.price = price;
+
+        return this;
+    }
+
+    public ProductBuilder withName(String name) {
+        this.name = name;
+
+        return this;
+    }
+
+    public ProductBuilder withProductType(ProductType productType) {
+        this.productType = productType;
+
+        return this;
+    }
+
+    public Product build() {
+        return new Product(id, price, name, productType);
+    }
 }
