d9a58792d801bbeb6ec6fc308fd90aae1e06b7cc
Adam Rezner <adam.rezner95@gmail.com>
2017-04-11 10:42:52 +0200

    RequestItemBuilder private variables added

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductDataBuilder.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductDataBuilder.java
@@ -1,9 +1,19 @@
 package pl.com.bottega.ecommerce.sales.domain.productscatalog;
 
+import java.util.Date;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.RequestItem;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.RequestItemBuilder;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class ProductDataBuilder {
+	
+	private Id productId = Id.generate();
+ 	private Money price = new Money(0);
+ 	private String name = "Adam";
+  	private Date snapshotDate = new Date();
+  	private ProductType type = ProductType.STANDARD;
 	
 	private ProductData productData = new ProductDataBuilder().build();
 		private int quantity = 1;
