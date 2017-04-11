5d900695a7e2af218930bc4bc5d8890721ccdc1f
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-04-11 14:24:11 +0200

    builder Add other methods in ReservationBuilder.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/reservation/ReservationBuilder.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/reservation/ReservationBuilder.java
@@ -6,6 +6,7 @@ import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductBuilder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -31,6 +32,49 @@ public class ReservationBuilder {
         this.removed = value;
 
         return this;
+    }
+
+    public ReservationBuilder withReservationStatus(Reservation.ReservationStatus status) {
+        this.status = status;
+
+        return this;
+    }
+
+    public ReservationBuilder withItems(List<ReservationItem> items) {
+        this.items = items;
+
+        return this;
+    }
+
+    public ReservationBuilder withItem(ReservationItem... item) {
+        this.items.addAll(Arrays.asList(item));
+
+        return this;
+    }
+
+    public ReservationBuilder withClientData(ClientData clientData) {
+        this.clientData = clientData;
+
+        return this;
+    }
+
+    public ReservationBuilder withCreateDate(Date createDate) {
+        this.createDate = createDate;
+
+        return this;
+    }
+
+    public Reservation build() {
+        Reservation reservation = new Reservation(id, status, clientData, createDate);
+
+        for (ReservationItem item : items) {
+            reservation.add(item.getProduct(), item.getQuantity());
+        }
+        if (removed) {
+            reservation.markAsRemoved();
+        }
+
+        return reservation;
     }
 
 
