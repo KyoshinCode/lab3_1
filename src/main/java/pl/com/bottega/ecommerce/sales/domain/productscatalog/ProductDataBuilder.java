dc67fb875f5652d47b688903ad5811b4f9cc90fc
Adam Rezner <adam.rezner95@gmail.com>
2017-04-11 10:38:46 +0200

    RequestItemBuilder: creating methods and attributes

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductDataBuilder.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductDataBuilder.java
@@ -1,5 +1,33 @@
 package pl.com.bottega.ecommerce.sales.domain.productscatalog;
 
+import pl.com.bottega.ecommerce.sales.domain.invoicing.RequestItem;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 public class ProductDataBuilder {
+	
+	private ProductData productData = new ProductDataBuilder().build();
+		private int quantity = 1;
+		private Money totalCost = new Money(1);
+	 	
+		public RequestItemBuilder() {}
+
+		public RequestItemBuilder withproductData(ProductData productData) {
+			this.productData = productData;
+			return this;
+		}
+		
+		public RequestItemBuilder withSnapshotDate(int quantity) {
+			this.quantity = quantity;
+			return this;
+		}
+		
+		public RequestItemBuilder withType(Money totalCost) {
+			this.totalCost = totalCost;
+			return this;
+		}
+		
+		public RequestItem build() {
+			return new RequestItem(productData, quantity, totalCost);
+		}
 
 }
