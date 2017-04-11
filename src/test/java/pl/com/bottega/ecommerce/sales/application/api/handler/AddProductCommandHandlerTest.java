660717290cde13670b1b50d41f84485ef2b0f84d
Przyjemski.Maciej <180264@edu.p.lodz.pl>
2017-04-11 13:29:08 +0200

    zmiana modyfikatora dostępu do klasy Product

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -12,8 +12,10 @@ import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation.ReservationStatus;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class AddProductCommandHandlerTest {
 
@@ -30,6 +32,9 @@ public class AddProductCommandHandlerTest {
 		AddProductCommand addProductCommand = new AddProductCommand(Id.generate(), Id.generate(), 5);
 		Reservation reservation = new Reservation(Id.generate(), ReservationStatus.OPENED,
 				new ClientData(Id.generate(), "Arleta"), new Date());
+		
+		Product product = new Product(Id.generate(), new Money(5), "sandwich", ProductType.FOOD);
+		
 	}
 
 }
