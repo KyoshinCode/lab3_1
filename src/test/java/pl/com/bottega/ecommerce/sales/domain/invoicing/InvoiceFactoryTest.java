28d2799ab595f6cbba38f792c556412e2c1ad5e6
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-04-23 19:57:58 +0200

    isolation Stworzono klase testowa dla InvoiceFactory

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryTest.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+public class InvoiceFactoryTest {
+
+}
