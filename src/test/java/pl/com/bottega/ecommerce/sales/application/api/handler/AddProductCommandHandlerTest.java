4d8468e6efe288302e00f3b6b1c09d8f2f2adeba
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-04-24 05:45:58 +0200

    Dodano klase testowa dla AddProductCommandHendler

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,32 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.Client;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+import pl.com.bottega.ecommerce.system.application.SystemUser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Rule;
+import org.mockito.Mock;
+import org.mockito.junit.*;
+
+
+public class AddProductCommandHandlerTest {
+	private AddProductCommandHandler addProductCommandHandler;
+	
+	@Test
+	public void test() {
+		addProductCommandHandler = new AddProductCommandHandler();
+	}
+
+}
