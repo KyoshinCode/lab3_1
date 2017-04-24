ca3b94ef9b26ea15e932151b11f10d79bdc6cb8c
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-04-24 02:59:43 +0200

    isolation Dodano test stanu dla metody create

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryTest.java
@@ -8,6 +8,27 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.*;
+
 public class InvoiceFactoryTest {
+	private InvoiceFactory invoiceFactory;
+	
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
+	
+	@Mock
+	ClientData dummyClientData;
+	
+	@Test public void testCreateInvoiceShouldCreateInvoice() {
+		invoiceFactory = new InvoiceFactory();
+		Invoice invoice = invoiceFactory.create(dummyClientData);
+		assertThat(invoice, instanceOf(Invoice.class));
+	}
 
 }
