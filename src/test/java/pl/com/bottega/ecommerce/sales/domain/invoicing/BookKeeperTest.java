7d577c88cb452eca1d62cb9caed2129fc1293d20
Khandree <a.lis.marciniak@gmail.com>
2017-06-19 21:43:35 +0200

    isolation Added test case Issuance With Correct Tax

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -17,6 +17,8 @@ import java.util.Date;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -49,5 +51,38 @@ public class BookKeeperTest {
         Invoice invoice = keeper.issuance(request, policy);
 
         assertThat(invoice.getItems().size(), is(equalTo(1)));
+    }
+
+    @Test
+    public void testIssuanceWithTwoItemsAndDoubleCalculateTax() throws Exception {
+        Money money = new Money(15);
+        ProductData[] data = new ProductData[2];
+        for(int i = 0; i < 2; i++) {
+            data[i] = new ProductData(Id.generate(), new Money(10 * i + 5), "stuff" + Integer.toString(i), ProductType.STANDARD, new Date());
+        }
+        when(policy.calculateTax(ProductType.STANDARD, money)).thenReturn(new Tax(new Money(10), "DummyType"));
+        InvoiceRequest request = new InvoiceRequest(new ClientData(Id.generate(), "Janina Kopytkowa"));
+        for(int i = 0; i < 2; i++) {
+            request.add(new RequestItem(data[i], i + 1, money));
+        }
+
+        keeper.issuance(request, policy);
+
+        verify(policy, times(2)).calculateTax(ProductType.STANDARD, money);
+    }
+
+    @Test
+    public void testIssuanceWithCorrectTax() throws Exception {
+        Money money = new Money(10);
+        Tax tax = new Tax(new Money(2.5), "FakeTax");
+
+        InvoiceRequest request = new InvoiceRequest(new ClientData(Id.generate(), "Jose Garcia"));
+        request.add(new RequestItem(new ProductData(Id.generate(), new Money(15), "stuff", ProductType.DRUG, new Date()) , 2, money));
+
+        when(policy.calculateTax(ProductType.DRUG, money)).thenReturn(tax);
+
+        Invoice invoice = keeper.issuance(request, policy);
+
+        assertThat(invoice.getItems().get(0).getTax(), is(equalTo(tax)));
     }
 }
