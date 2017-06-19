68ce1fce0ac2f2f2ec0a7cf7986a353e476f595f
Khandree <a.lis.marciniak@gmail.com>
2017-06-19 23:03:53 +0200

    Added ProductBuilder and ReservationBuilder; Added HandleWithCheckReservationRepositoryWithAggregateIdOrWithClient test

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -2,17 +2,24 @@ package pl.com.bottega.ecommerce.sales.application.api.handler;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.internal.util.reflection.Whitebox;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
 import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
 import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
 import pl.com.bottega.ecommerce.system.application.SystemContext;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Agata on 2017-06-19.
@@ -34,6 +41,8 @@ public class AddProductCommandHandlerTest {
 
     private AddProductCommandHandler handler;
 
+    private AddProductCommand command;
+
     @Before
     public void setUp() throws Exception {
         handler = new AddProductCommandHandler();
@@ -42,5 +51,20 @@ public class AddProductCommandHandlerTest {
         Whitebox.setInternalState(handler, "suggestionService", suggestionService);
         Whitebox.setInternalState(handler, "clientRepository", clientRepository);
         Whitebox.setInternalState(handler, "systemContext", systemContext);
+
+        command = new AddProductCommand(Id.generate(), Id.generate(), 5);
+    }
+
+    @Test
+    public void testHandleWithCheckReservationRepositoryWithAggregateIdOrWithClient() throws Exception {
+        Reservation reservation = new ReservationBuilder().withClient(new ClientData(Id.generate(), "dummy")).opened().build();
+        Product product = new ProductBuilder().withAggregateId(command.getProductId()).build();
+
+        when(reservationRepository.load(command.getOrderId())).thenReturn(reservation);
+        when(productRepository.load(command.getProductId())).thenReturn(product);
+
+        handler.handle(command);
+
+        verify(reservationRepository, times(1)).load(command.getOrderId());
     }
 }
